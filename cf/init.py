bbb2cfb2c9db554539022c74d91130527abce252
Bogdan Sinitsyn <bogdan.sinitsyn@gmail.com>
2017-10-16 13:45:53 +0300

    Use tasks number

--- cf/init.py
+++ cf/init.py
@@ -2,17 +2,18 @@ import shutil
 from cf.config import global_conf, global_conf_dir
 
 
-def make_new_workdir(round_num):
+def make_new_workdir(round_num, tasks_num):
     round_skeleton_dir = f'{global_conf_dir}/round-skeleton/'
     shutil.copytree(round_skeleton_dir, round_num)
     ext = global_conf['all.extension']
     skeleton_filename = f'{global_conf_dir}/file-skeletons/skeleton.{ext}'
-    for task in 'abcde':
+    for i in range(tasks_num):
+        task = chr(ord('a') + i)
         shutil.copy(skeleton_filename, f'{round_num}/{task}.{ext}')
 
 
 def parse_args(argv):
-    make_new_workdir(argv[1])
+    make_new_workdir(argv[1], int(argv[2]))
 
 
 __all__ = ['parse_args']
