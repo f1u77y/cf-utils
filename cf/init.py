0c557443bf9684c83113466db0ad5c158b679157
Bogdan Sinitsyn <f1u77y@yandex.ru>
2019-02-17 22:41:50 +0300

    Determine number of problems from contest ID if possible

--- cf/init.py
+++ cf/init.py
@@ -1,25 +1,29 @@
 import shutil
 
 from cf.config import global_conf, global_conf_dir
+import cf.util
 
 
-def make_new_workdir(round_num, tasks_num):
+def make_new_workdir(round_num, problems_num):
     round_skeleton_dir = f'{global_conf_dir}/round-skeleton/'
     shutil.copytree(round_skeleton_dir, round_num)
     ext = global_conf['all.extension']
     skeleton_filename = f'{global_conf_dir}/file-skeletons/skeleton.{ext}'
-    for i in range(tasks_num):
+    for i in range(problems_num):
         task = chr(ord('a') + i)
         shutil.copy(skeleton_filename, f'{round_num}/{task}.{ext}')
 
 
 def run(args):
-    make_new_workdir(args.round_num, args.tasks_num)
+    problems_num = args.problems_num
+    if problems_num == -1:
+        problems_num = cf.util.get_problems_count(args.round_num)
+    make_new_workdir(args.round_num, problems_num)
 
 
 def parse_args(parser):
     parser.add_argument('round_num', metavar='ROUND-NUMBER')
-    parser.add_argument('tasks_num', metavar='TASKS-NUMBER', type=int)
+    parser.add_argument('problems_num', metavar='PROBLEMS-NUMBER', type=int, nargs='?', default=-1)
     parser.set_defaults(run=run)
 
 
